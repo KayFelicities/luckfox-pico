d8b89e3de369f6c9e071dffaa9ef444de1c95079
Kay@thinkpad-wsl <kayfelicities@outlook.com>
2024-09-04 19:22:38 +0800

    update

--- configBusybox.sh
+++ configBusybox.sh
@@ -2,4 +2,5 @@
 
 cp -a kayPatch/busybox.config sysdrv/source/buildroot/buildroot-2023.02.6/output/build/busybox-1.36.1/.config \
 && (cd sysdrv/source/buildroot/buildroot-2023.02.6/output/build/busybox-1.36.1 && make menuconfig) \
-&& cp -a sysdrv/source/buildroot/buildroot-2023.02.6/output/build/busybox-1.36.1/.config kayPatch/busybox.config
+&& cp -a sysdrv/source/buildroot/buildroot-2023.02.6/output/build/busybox-1.36.1/.config kayPatch/busybox.config \
+&& cp -a kayPatch/busybox.config sysdrv/source/buildroot/buildroot-2023.02.6/package/busybox/busybox.config
