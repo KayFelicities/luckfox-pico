ea8157ad5489ee566dda82909c117d3155121291
Kay@thinkpad-wsl <kayfelicities@outlook.com>
2024-09-04 15:27:50 +0800

    update patch

--- install.sh
+++ install.sh
@@ -10,10 +10,17 @@ rm -f output/out/rootfs_uclibc_rv1106/etc/init.d/S99rtcinit
 
 cp -a kayPatch/S99app output/out/rootfs_uclibc_rv1106/etc/init.d/
 cp -a kayPatch/S50usbdevice output/out/rootfs_uclibc_rv1106/etc/init.d/
+chmod +x output/out/rootfs_uclibc_rv1106/etc/init.d/*
+
 cp -a kayPatch/app.sh output/out/rootfs_uclibc_rv1106/etc/
-cp -a kayPatch/libguiFont.so kayPatch/libcurl.so.4 output/out/rootfs_uclibc_rv1106/lib/
+
+cp -a kayPatch/libguiFont.so output/out/rootfs_uclibc_rv1106/lib/
+cp -a kayPatch/libcurl.so.4 output/out/rootfs_uclibc_rv1106/lib/
+
 mkdir -p output/out/userdata/apps/
-cp -a kayPatch/lcd kayPatch/reader output/out/userdata/apps/
+cp -a kayPatch/lcd output/out/userdata/apps/
+cp -a kayPatch/reader output/out/userdata/apps/
+
 mkdir -p output/out/userdata/modules/
 cp -a kayPatch/soft_uart.ko output/out/userdata/modules/
 
