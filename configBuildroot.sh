422f7401305d549541f1895f3289d3b621041c71
Kay@homewsl <kayfelicities@outlook.com>
2024-09-28 23:43:07 +0800

    set gpio

--- configBuildroot.sh
+++ configBuildroot.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 cp -a kayPatch/buildroot.config sysdrv/source/buildroot/buildroot-2023.02.6/.config \
-&& ./build.sh buildrootconfig \
+&& ( cd /home/kay/workspace/luckfox-pico/sysdrv/source/buildroot/buildroot-2023.02.6 && make menuconfig && make savedefconfig && make ) \
 && cp -a sysdrv/source/buildroot/buildroot-2023.02.6/.config kayPatch/buildroot.config
